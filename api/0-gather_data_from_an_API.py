8359be169619510b05199a93e358e428842dc026
EvaMicich <6366@holbertonstudents.com>
2023-08-22 04:46:59 -0700

    pep8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,8 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
     user = requests.get(f"{url}/users/{sys.argv[1]}").json()
-    to_dos = requests.get(f"{url}/todos", params={"userId": sys.argv[1]}).json()
+    to_dos = requests.get(f"{url}/todos",
+                          params={"userId": sys.argv[1]}).json()
     total_tasks = len(to_dos)
     completed_tasks = sum(1 for to_do in to_dos if to_do["completed"])
 
